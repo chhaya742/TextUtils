405c84c0421335edae209ccc9c4d7cc38bcd18ce
chhaya.Bagwan <chhaya.bagwan@cognisun.com>
2022-09-08 18:46:39 +0530

    changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,7 @@ export default function NavBar(props) {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
       <a className="navbar-brand" href="/">{props.Title}</a>
+       {/* <Link className="navbar-brand" to="/">{props.Title}</Link> */}
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -14,9 +15,11 @@ export default function NavBar(props) {
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="/">Home</a>
+             {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
           </li>
           <li className="nav-item">
             <a className="nav-link" href="/">{props.About}</a>
+            
           </li>
         </ul>
          
